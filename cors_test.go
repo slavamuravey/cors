dbe621830a50891b1f3666b8f37dd420af2c3d1f
slavamuravey <slavamuravey@mail.ru>
2020-06-04 02:44:40 +0700

    Add tests

--- cors_test.go
+++ cors_test.go
@@ -22,7 +22,7 @@ func TestEmptyAllowOrigin(t *testing.T) {
     method: "GET",
     reqHeaders: http.Header{
       "Origin": []string{
-        "http://example.com/",
+        "http://foo.example.com",
       },
     },
     resHeaders: http.Header{
@@ -44,7 +44,7 @@ func TestAllowAllOrigin(t *testing.T) {
     method: "GET",
     reqHeaders: http.Header{
       "Origin": []string{
-        "http://example.com/",
+        "http://foo.example.com",
       },
     },
     resHeaders: http.Header{
@@ -54,6 +54,335 @@ func TestAllowAllOrigin(t *testing.T) {
         "Access-Control-Request-Headers",
       },
       "Access-Control-Allow-Origin": []string{"*"},
+    },
+    statusCode: 200,
+  })
+}
+
+func TestNotCorsRequest(t *testing.T) {
+  testTestCase(t, &testCase{
+    config: &Config{
+    },
+    method: "GET",
+    reqHeaders: http.Header{
+      "Origin": []string{
+        "http://example.com",
+      },
+    },
+    resHeaders: http.Header{
+      "Vary": []string{
+        "Origin",
+        "Access-Control-Request-Method",
+        "Access-Control-Request-Headers",
+      },
+    },
+    statusCode: 200,
+  })
+}
+
+func TestAllowConcreteOrigin(t *testing.T) {
+  testTestCase(t, &testCase{
+    config: &Config{
+      AllowOriginPattern: "^http://foo.example.com$",
+    },
+    method: "GET",
+    reqHeaders: http.Header{
+      "Origin": []string{
+        "http://foo.example.com",
+      },
+    },
+    resHeaders: http.Header{
+      "Vary": []string{
+        "Origin",
+        "Access-Control-Request-Method",
+        "Access-Control-Request-Headers",
+      },
+      "Access-Control-Allow-Origin": []string{"http://foo.example.com"},
+    },
+    statusCode: 200,
+  })
+}
+
+func TestCantParseOrigin(t *testing.T) {
+  testTestCase(t, &testCase{
+    config: &Config{
+      AllowOriginPattern: string([]byte{227}),
+    },
+    method: "GET",
+    reqHeaders: http.Header{
+      "Origin": []string{
+        "http://foo.example.com",
+      },
+    },
+    resHeaders: http.Header{
+      "Vary": []string{
+        "Origin",
+        "Access-Control-Request-Method",
+        "Access-Control-Request-Headers",
+      },
+    },
+    statusCode: 500,
+  })
+}
+
+func TestExposeHeadersAllowCredentials(t *testing.T) {
+  testTestCase(t, &testCase{
+    config: &Config{
+      AllowAllOrigin:   true,
+      AllowCredentials: true,
+      ExposedHeaders:   []string{"Header1", "Header2"},
+    },
+    method: "GET",
+    reqHeaders: http.Header{
+      "Origin": []string{
+        "http://foo.example.com",
+      },
+    },
+    resHeaders: http.Header{
+      "Vary": []string{
+        "Origin",
+        "Access-Control-Request-Method",
+        "Access-Control-Request-Headers",
+      },
+      "Access-Control-Allow-Origin":      []string{"*"},
+      "Access-Control-Allow-Credentials": []string{"true"},
+      "Access-Control-Expose-Headers":    []string{"Header1,Header2"},
+    },
+    statusCode: 200,
+  })
+}
+
+func TestPreflightFail(t *testing.T) {
+  testTestCase(t, &testCase{
+    config: &Config{
+      AllowAllOrigin: true,
+      MaxAge:         1000,
+    },
+    method: "OPTIONS",
+    reqHeaders: http.Header{
+      "Origin": []string{
+        "http://foo.example.com",
+      },
+      "Access-Control-Request-Method": []string{
+        "DELETE",
+      },
+    },
+    resHeaders: http.Header{
+      "Vary": []string{
+        "Origin",
+        "Access-Control-Request-Method",
+        "Access-Control-Request-Headers",
+      },
+      "Access-Control-Allow-Origin": []string{"*"},
+    },
+    statusCode: 405,
+  })
+}
+
+func TestPreflightMethodNotInUpperCase(t *testing.T) {
+  testTestCase(t, &testCase{
+    config: &Config{
+      AllowAllOrigin: true,
+      MaxAge:         1000,
+      AllowMethods:   []string{"LINK"},
+    },
+    method: "OPTIONS",
+    reqHeaders: http.Header{
+      "Origin": []string{
+        "http://foo.example.com",
+      },
+      "Access-Control-Request-Method": []string{
+        "Link",
+      },
+    },
+    resHeaders: http.Header{
+      "Vary": []string{
+        "Origin",
+        "Access-Control-Request-Method",
+        "Access-Control-Request-Headers",
+      },
+      "Access-Control-Allow-Origin":  []string{"*"},
+      "Access-Control-Max-Age":       []string{"1000"},
+      "Access-Control-Allow-Methods": []string{"LINK,Link"},
+    },
+    statusCode: 200,
+  })
+}
+
+func TestPreflightTerminationStatus(t *testing.T) {
+  testTestCase(t, &testCase{
+    config: &Config{
+      AllowAllOrigin:             true,
+      AllowMethods:               []string{"PUT"},
+      PreflightTerminationStatus: 204,
+    },
+    method: "OPTIONS",
+    reqHeaders: http.Header{
+      "Origin": []string{
+        "http://foo.example.com",
+      },
+      "Access-Control-Request-Method": []string{
+        "PUT",
+      },
+    },
+    resHeaders: http.Header{
+      "Vary": []string{
+        "Origin",
+        "Access-Control-Request-Method",
+        "Access-Control-Request-Headers",
+      },
+      "Access-Control-Allow-Origin":  []string{"*"},
+      "Access-Control-Allow-Methods": []string{"PUT"},
+    },
+    statusCode: 204,
+  })
+}
+
+func TestContinuousPreflight(t *testing.T) {
+  var nextFuncInvoked bool
+  nextFunc := func(w http.ResponseWriter, r *http.Request) {
+    nextFuncInvoked = true
+  }
+  hf := CreateHandlerFunc(&Config{
+    AllowAllOrigin:      true,
+    AllowMethods:        []string{"PUT"},
+    ContinuousPreflight: true,
+  })(http.HandlerFunc(nextFunc))
+  res := httptest.NewRecorder()
+  req, _ := http.NewRequest("OPTIONS", "http://example.com/foo", nil)
+  req.Header = http.Header{
+    "Origin": []string{
+      "http://foo.example.com",
+    },
+    "Access-Control-Request-Method": []string{
+      "PUT",
+    },
+  }
+  hf(res, req)
+
+  assertTrue(t, nextFuncInvoked, "Next function must be invoked")
+}
+
+func TestNotContinuousPreflight(t *testing.T) {
+  var nextFuncInvoked bool
+  nextFunc := func(w http.ResponseWriter, r *http.Request) {
+    nextFuncInvoked = true
+  }
+  hf := CreateHandlerFunc(&Config{
+    AllowAllOrigin:      true,
+    AllowMethods:        []string{"PUT"},
+    ContinuousPreflight: false,
+  })(http.HandlerFunc(nextFunc))
+  res := httptest.NewRecorder()
+  req, _ := http.NewRequest("OPTIONS", "http://example.com/foo", nil)
+  req.Header = http.Header{
+    "Origin": []string{
+      "http://foo.example.com",
+    },
+    "Access-Control-Request-Method": []string{
+      "PUT",
+    },
+  }
+  hf(res, req)
+
+  assertFalse(t, nextFuncInvoked, "Next function mustn't be invoked")
+}
+
+func TestAllowHeadersFailure(t *testing.T) {
+  testTestCase(t, &testCase{
+    config: &Config{
+      AllowAllOrigin: true,
+      AllowMethods:   []string{"PUT"},
+      AllowHeaders:   []string{"Header1", "Header2"},
+    },
+    method: "OPTIONS",
+    reqHeaders: http.Header{
+      "Origin": []string{
+        "http://foo.example.com",
+      },
+      "Access-Control-Request-Method": []string{
+        "PUT",
+      },
+      "Access-Control-Request-Headers": []string{
+        "Header3,Header4",
+      },
+    },
+    resHeaders: http.Header{
+      "Vary": []string{
+        "Origin",
+        "Access-Control-Request-Method",
+        "Access-Control-Request-Headers",
+      },
+      "Access-Control-Allow-Origin":  []string{"*"},
+      "Access-Control-Allow-Methods": []string{"PUT"},
+      "Access-Control-Allow-Headers": []string{"Header1,Header2"},
+    },
+    statusCode: 400,
+  })
+}
+
+func TestAllowHeadersSuccess(t *testing.T) {
+  testTestCase(t, &testCase{
+    config: &Config{
+      AllowAllOrigin: true,
+      AllowMethods:   []string{"PUT"},
+      AllowHeaders:   []string{"Header1", "Header2"},
+    },
+    method: "OPTIONS",
+    reqHeaders: http.Header{
+      "Origin": []string{
+        "http://foo.example.com",
+      },
+      "Access-Control-Request-Method": []string{
+        "PUT",
+      },
+      "Access-Control-Request-Headers": []string{
+        "Header1,Header2",
+      },
+    },
+    resHeaders: http.Header{
+      "Vary": []string{
+        "Origin",
+        "Access-Control-Request-Method",
+        "Access-Control-Request-Headers",
+      },
+      "Access-Control-Allow-Origin":  []string{"*"},
+      "Access-Control-Allow-Methods": []string{"PUT"},
+      "Access-Control-Allow-Headers": []string{"Header1,Header2"},
+    },
+    statusCode: 200,
+  })
+}
+
+func TestAllowAllHeaders(t *testing.T) {
+  testTestCase(t, &testCase{
+    config: &Config{
+      AllowAllOrigin:  true,
+      AllowMethods:    []string{"PUT"},
+      AllowHeaders:    []string{"Header1", "Header2"},
+      AllowAllHeaders: true,
+    },
+    method: "OPTIONS",
+    reqHeaders: http.Header{
+      "Origin": []string{
+        "http://foo.example.com",
+      },
+      "Access-Control-Request-Method": []string{
+        "PUT",
+      },
+      "Access-Control-Request-Headers": []string{
+        "Header3,Header4",
+      },
+    },
+    resHeaders: http.Header{
+      "Vary": []string{
+        "Origin",
+        "Access-Control-Request-Method",
+        "Access-Control-Request-Headers",
+      },
+      "Access-Control-Allow-Origin":  []string{"*"},
+      "Access-Control-Allow-Methods": []string{"PUT"},
+      "Access-Control-Allow-Headers": []string{"Header3,Header4"},
     },
     statusCode: 200,
   })
